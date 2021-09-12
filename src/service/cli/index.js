676883651c1191c36dec2574823b4beff7a055ef
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-12 15:54:14 +0700

    feat: Подключение и экспорт модулей

---
+++ src/service/cli/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const generateModule = require(`./generate`);
+const versionModule = require(`./version`);
+const helpModule = require(`./help`);
+
+module.exports = {
+  generateModule,
+  versionModule,
+  helpModule
+};
